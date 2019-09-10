fb748b8469bef841ff985ff99fb77fddadedb303
vismay2711 <visu.patel11@gmail.com>
2019-09-10 11:40:05 +0530

    final game is created with core game logic and quit functionality.

---
+++ magic_number_UI/Assets/Scripts/ScenePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePlayer : MonoBehaviour
+{
+   public void sceneLoader()
+    {
+        int currentSIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSIndex + 1);
+    }
+
+    public void goToStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ magic_number_UI/Assets/Scripts/magicNumber.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class magicNumber : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessNum;
+    int guess, lastGuess;
+
+    List<int> guessList = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max +1);
+        guessNum.text = guess.ToString();
+    }
+
+    public void higherPressed()
+    {
+        min = guess +1;
+        NextGuess();
+    }
+
+    public void lowerPressed()
+    {
+        max = guess -1;
+        NextGuess();
+    }
+}
